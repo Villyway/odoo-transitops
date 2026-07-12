a9a138f27f8818ef96e236e24fb82fd59bd15e55
santosh mehta <santoshmehta799@gmail.com>
2026-07-12 11:23:28 +0530

    Added project app drivers

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Driver
 
-# Register your models here.
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ("name", "license_number", "license_expiry_date", "status", "safety_score")
+    list_filter = ("status", "license_category")
+    search_fields = ("name", "license_number")
